138bcef52acdba06fbeca089d100fbaa05237635
HKprogram <ricknogueira1231@gmail.com>
2022-04-01 11:52:14 -0300

    Utilizando MobX para gerenciar estados

---
+++ lib/controller.dart
@@ -0,0 +1,18 @@
+import 'package:mobx/mobx.dart';
+
+class Controller {
+  var _contador = Observable(0);
+
+  Action? incrementar;
+
+  int get contador => _contador.value;
+  set contador(int value) => _contador.value = value;
+
+  Controller() {
+    incrementar = Action(_incrementar);
+  }
+
+  _incrementar() {
+    contador = contador + 1;
+  }
+}
---
+++ lib/home.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:mobx_aula/controller.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  // int _counter = 0;
+
+  // void _incrementCounter() {
+  //   setState(() {
+  //     _counter++;
+  //   });
+  // }
+
+  Controller controller = Controller();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Observer(builder: (_) {
+              return Text(
+                '${controller.contador}',
+                style: Theme.of(context).textTheme.headline4,
+              );
+            }),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          controller.incrementar!();
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
